bea57756fbc254b5b0912252290285eb74699b8c
Hubert Lipinski <hubertlipinskipl@gmail.com>
2020-07-29 10:48:08 +0200

    / route & fix

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,17 @@ const router = express.Router();
 
 const Url = require('../models/Url');
 
+// @route   GET /
+router.get('/', (req, res) => {
+    res.json({
+        response: 'shrt.pl - be shrt and quick!', 
+        info: {
+            author: 'Hubert Lipiński',
+            repo: 'https://github.com/HubertLipinski/shortUrl'
+        }
+    })
+})
+
 // @route   GET /:code
 router.get('/:code', async (req, res) => {
     try {
